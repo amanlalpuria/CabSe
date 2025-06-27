ca9be0734046d4306d4745440fe333512c4dc1c6
Aman Lalpuria <aman.sharma163@gmail.com>
2025-06-27 20:43:15 +0530

    Inital commit

---
+++ src/main/java/com/vedvix/cabse/CabseApplication.java
@@ -0,0 +1,13 @@
+package com.vedvix.cabse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedvix/cabse/CabseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedvix.cabse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
